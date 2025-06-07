c1c1c1b888915ade7ea725dec9d070f027ff8fd2
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-07 11:02:49 +0700

    feat: add base docker configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,1 @@
-export const configExtends = ['@commitlint/config-conventional'];
-export const rules = {
-  //   TODO Add Scope Enum Here
-  // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-  'type-enum': [
-    2,
-    'always',
-    [
-      'feat',
-      'fix',
-      'docs',
-      'delate',
-      'chore',
-      'refactor',
-      'ci',
-    ],
-  ],
-};
+export default { extends: ['@commitlint/config-conventional'] }

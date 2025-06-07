5a9000374d58387129eb04f815a31d9480232c23
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-07 11:03:22 +0700

    feat: add base commitlint cli config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
 export default { extends: ['@commitlint/config-conventional'] }
+export const rules = {
+  'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'delate', 'chore', 'refactor', 'ci']],
+}
